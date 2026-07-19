60a725c3865b498859e46715e5209623d82e56e3
Mr-Pineapple <44590089+Mr-Pineapple@users.noreply.github.com>
2026-07-19 15:46:32 +0100

    🚧 Implemented callback event for block interaction

--- src/main/java/com/mrpineapple/fletcher/Fletcher.java
+++ src/main/java/com/mrpineapple/fletcher/Fletcher.java
@@ -2,8 +2,13 @@ package com.mrpineapple.fletcher;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.minecraft.core.BlockPos;
 import net.minecraft.resources.Identifier;
 
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.level.block.Blocks;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,7 +18,22 @@ public class Fletcher implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
+			BlockPos blockPosition = hitResult.getBlockPos();
+			if(!world.getBlockState(blockPosition).is(Blocks.FLETCHING_TABLE)) {
+				return InteractionResult.PASS;
+			}
+
+			if(world.isClientSide()) {
+				return InteractionResult.SUCCESS;
+			}
+
+			if(player instanceof ServerPlayer serverPlayer) {
+				System.out.println("FLETCHER");
+				return InteractionResult.CONSUME;
+			}
+			return InteractionResult.PASS;
+		});
 	}
 
 	public static Identifier id(String path) {
