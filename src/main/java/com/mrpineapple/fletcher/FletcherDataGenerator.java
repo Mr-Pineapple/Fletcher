464d1aea7c8ec758dd8fd52da4c2584005e71944
Mr-Pineapple <44590089+Mr-Pineapple@users.noreply.github.com>
2026-07-19 11:57:54 +0100

    🎉 Created Project

---
+++ src/main/java/com/mrpineapple/fletcher/FletcherDataGenerator.java
@@ -0,0 +1,11 @@
+package com.mrpineapple.fletcher;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FletcherDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
