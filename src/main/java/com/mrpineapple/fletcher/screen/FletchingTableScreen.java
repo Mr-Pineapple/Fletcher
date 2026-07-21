d7389242d99523f710332a00ccb0e2006a6fc811
Mr-Pineapple <zackbird1@outlook.com>
2026-07-21 11:29:21 +0100

    💫 Slot background cycles
    Modifier slot now cycles between dust and potion - similar to smithing table

--- src/main/java/com/mrpineapple/fletcher/screen/FletchingTableScreen.java
+++ src/main/java/com/mrpineapple/fletcher/screen/FletchingTableScreen.java
@@ -3,14 +3,21 @@ package com.mrpineapple.fletcher.screen;
 import com.mrpineapple.fletcher.Fletcher;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.client.gui.screens.inventory.CyclingSlotBackground;
 import net.minecraft.client.renderer.RenderPipelines;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
 import net.minecraft.world.entity.player.Inventory;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
+
 public class FletchingTableScreen extends AbstractContainerScreen<@NotNull FletchingTableMenu> {
-    public static final Identifier FLETCHING_TABLE_LOCATION = Identifier.fromNamespaceAndPath(Fletcher.MOD_ID, "textures/gui/fletching.png");
+    private static final Identifier FLETCHING_TABLE_LOCATION = Identifier.fromNamespaceAndPath(Fletcher.MOD_ID, "textures/gui/fletching.png");
+    private static final Identifier EMPTY_SLOT_GLOWSTONE = Identifier.withDefaultNamespace("container/slot/redstone_dust");
+    private static final Identifier EMPTY_SLOT_POTION = Identifier.withDefaultNamespace("container/slot/potion");
+    private static final List<Identifier> EMPTY_MODIFIER_SLOTS = List.of(EMPTY_SLOT_GLOWSTONE, EMPTY_SLOT_POTION);
+    private final CyclingSlotBackground modifierIcon = new CyclingSlotBackground(1);
 
     public FletchingTableScreen(@NotNull FletchingTableMenu menu, Inventory inventory, Component title) {
         super(menu, inventory, title);
@@ -23,5 +30,12 @@ public class FletchingTableScreen extends AbstractContainerScreen<@NotNull Fletc
         int y = (height - imageHeight) / 2;
 
         graphics.blit(RenderPipelines.GUI_TEXTURED, FLETCHING_TABLE_LOCATION, x, y, 0.0F, 0.0F, imageWidth, imageHeight, 256, 256);
+        this.modifierIcon.extractRenderState(this.menu, graphics, delta, this.leftPos, this.topPos);
+    }
+
+    @Override
+    protected void containerTick() {
+        super.containerTick();
+        this.modifierIcon.tick(EMPTY_MODIFIER_SLOTS);
     }
 }
